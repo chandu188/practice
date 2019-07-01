a9d0066a38f91d2318bcc47cbd537068d3ec3537
Sathish Gampa <sathish.gampa@zalando.de>
2019-07-01 11:22:26 +0200

    fixed terenary tries and added search operations

--- datastructures/trie/terenary_trie.go
+++ datastructures/trie/terenary_trie.go
@@ -13,49 +13,97 @@ type TerenaryTrie struct {
 }
 
 func (tt *TerenaryTrie) Get(key string) int {
-	return getValue(tt.root, key, 0)
-}
-
-func getValue(tn *terenaryTrieNode, key string, index int) int {
-	if tn == nil {
+	n := getValue(tt.root, key, 0)
+	if n == nil {
 		return -1
 	}
+	return n.value
+}
 
-	if index == len(key)-1 {
-		return tn.value
+func getValue(tn *terenaryTrieNode, key string, index int) *terenaryTrieNode {
+	if tn == nil {
+		return nil
 	}
-
-	if tn.ch == key[index] {
+	ch := key[index]
+	if ch < tn.ch {
+		return getValue(tn.left, key, index)
+	} else if ch > tn.ch {
+		return getValue(tn.right, key, index)
+	} else if index < len(key)-1 {
 		return getValue(tn.middle, key, index+1)
-	} else if key[index] < tn.ch {
-		return getValue(tn.left, key, index+1)
+	} else {
+		return tn
 	}
-	return getValue(tn.right, key, index+1)
 }
 
 func (tt *TerenaryTrie) Put(key string, value int) {
 	tt.root = putValue(tt.root, key, value, 0)
 }
 
-func newTerenaryTrieNode() *terenaryTrieNode {
-	return &terenaryTrieNode{}
-}
-
 func putValue(tn *terenaryTrieNode, key string, value int, index int) *terenaryTrieNode {
+	ch := key[index]
 	if tn == nil {
-		tn = newTerenaryTrieNode()
+		tn = &terenaryTrieNode{}
+		tn.ch = ch
 	}
-	if index == len(key)-1 {
-		tn.value = value
-		tn.ch = key[index]
-		return tn
-	}
-	if tn.ch == key[index] {
+	if ch < tn.ch {
+		tn.left = putValue(tn.left, key, value, index)
+	} else if ch > tn.ch {
+		tn.right = putValue(tn.right, key, value, index)
+	} else if index < len(key)-1 {
 		tn.middle = putValue(tn.middle, key, value, index+1)
-	} else if key[index] < tn.ch {
-		tn.left = putValue(tn.left, key, value, index+1)
 	} else {
-		tn.right = putValue(tn.right, key, value, index+1)
+		tn.value = value
 	}
 	return tn
+}
+
+func (tt *TerenaryTrie) Keys() []string {
+	keys := make([]string, 0)
+	return collect(tt.root, "", keys)
+}
+
+func (tt *TerenaryTrie) KeysWithPrefix(str string) []string {
+	n := getValue(tt.root, str, 0)
+	keys := make([]string, 0)
+	return collect(n.middle, "", keys)
+}
+
+func (tt *TerenaryTrie) LongestPrefixOf(key string) string {
+	len := searchFor(tt.root, key, 0, 0)
+	return key[0:len]
+}
+func searchFor(tn *terenaryTrieNode, key string, index int, length int) int {
+	if tn == nil {
+		return length
+	}
+	if tn.value != 0 {
+		length = index + 1
+	}
+	if index == len(key)-1 {
+		return len(key)
+	}
+	ch := key[index]
+	if ch < tn.ch {
+		return searchFor(tn.left, key, index, length)
+	} else if ch > tn.ch {
+		return searchFor(tn.right, key, index, length)
+	} else {
+		return searchFor(tn.middle, key, index+1, length)
+	}
+}
+
+func collect(tt *terenaryTrieNode, prefix string, keys []string) []string {
+	if tt == nil {
+		return keys
+	}
+
+	keys = collect(tt.left, prefix, keys)
+	keys = collect(tt.right, prefix, keys)
+	keys = collect(tt.middle, prefix+string(tt.ch), keys)
+
+	if tt.value != 0 {
+		keys = append(keys, prefix)
+	}
+	return keys
 }
--- datastructures/trie/terneary_trie_test.go
+++ datastructures/trie/terneary_trie_test.go
@@ -13,6 +13,62 @@ func TestTerenaryTrie(t *testing.T) {
 	}{
 		{
 			key:   "she",
+			value: 10,
+		},
+		{
+			key:   "sells",
+			value: 1,
+		},
+		{
+			key:   "sea",
+			value: 2,
+		},
+		{
+			key:   "shells",
+			value: 3,
+		},
+		{
+			key:   "by",
+			value: 4,
+		},
+		{
+			key:   "the",
+			value: 5,
+		},
+		{
+			key:   "sea",
+			value: 6,
+		},
+		{
+			key:   "shore",
+			value: 7,
+		},
+	}
+	for _, in := range input {
+		tt.Put(in.key, in.value)
+	}
+
+	testCases := []struct {
+		key   string
+		value int
+	}{
+		{key: "sea", value: 6}, {key: "shelter", value: -1},
+	}
+
+	for _, tc := range testCases {
+		act := tt.Get(tc.key)
+		assert.Equal(t, tc.value, act)
+	}
+}
+
+func TestTerenaryTrieKeys(t *testing.T) {
+	tt := &TerenaryTrie{}
+	input := []struct {
+		key   string
+		value int
+	}{
+		{
+			key:   "she",
 			value: 0,
 		},
 		{
@@ -45,16 +101,104 @@ func TestTerenaryTrie(t *testing.T) {
 	for _, in := range input {
 		tt.Put(in.key, in.value)
 	}
+	res := []string{"she", "sea", "shells", "by", "the", "shore"}
+	keys := tt.Keys()
+	assert.Equal(t, len(res), len(keys))
+}
 
-	testCases := []struct {
+func TestTerenaryTrieKeysWithPrefix(t *testing.T) {
+	tt := &TerenaryTrie{}
+	input := []struct {
 		key   string
 		value int
 	}{
-		{key: "sea", value: 6}, {key: "shelter", value: -1},
+		{
+			key:   "she",
+			value: 10,
+		},
+		{
+			key:   "sells",
+			value: 1,
+		},
+		{
+			key:   "sea",
+			value: 2,
+		},
+		{
+			key:   "shells",
+			value: 3,
+		},
+		{
+			key:   "by",
+			value: 4,
+		},
+		{
+			key:   "the",
+			value: 5,
+		}, {
+			key:   "sea",
+			value: 6,
+		},
+		{
+			key:   "shore",
+			value: 7,
+		},
 	}
+	for _, in := range input {
+		tt.Put(in.key, in.value)
+	}
+	res := []string{"she", "shells", "shore"}
+	keys := tt.KeysWithPrefix("sh")
+	assert.Equal(t, len(res), len(keys))
+}
 
-	for _, tc := range testCases {
-		act := tt.Get(tc.key)
-		assert.Equal(t, tc.value, act)
+func TestLongestKey(t *testing.T) {
+	tt := &TerenaryTrie{}
+	input := []struct {
+		key   string
+		value int
+	}{
+		{
+			key:   "she",
+			value: 10,
+		},
+		{
+			key:   "sells",
+			value: 1,
+		},
+		{
+			key:   "sea",
+			value: 2,
+		},
+		{
+			key:   "shells",
+			value: 3,
+		},
+		{
+			key:   "by",
+			value: 4,
+		},
+		{
+			key:   "the",
+			value: 5,
+		}, {
+			key:   "sea",
+			value: 6,
+		},
+		{
+			key:   "shore",
+			value: 7,
+		},
 	}
+	for _, in := range input {
+		tt.Put(in.key, in.value)
+	}
+	longKey := tt.LongestPrefixOf("shell")
+	assert.Equal(t, "shell", longKey)
+	longKey = tt.LongestPrefixOf("she")
+	assert.Equal(t, "she", longKey)
+	longKey = tt.LongestPrefixOf("shellsort")
+	assert.Equal(t, "shells", longKey)
+	longKey = tt.LongestPrefixOf("by")
+	assert.Equal(t, "by", longKey)
 }
